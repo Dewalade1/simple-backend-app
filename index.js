8c3d08945dba39dfb44fd2c4dcf27269734e9173
Dewa <adewalade@gmail.com>
2021-06-17 15:20:45 +0100

    chore: add swagger documentation for test route

--- index.js
+++ index.js
@@ -39,8 +39,41 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // TEST ENDPOINT
 app.use('/test', (req, res) => {
     console.log("Congrats! Your test route works!!!");
-    return res.status(200).send('Test Successful');
+    return res.status(200).send('Test Successful. Welcome to the first-API backend!!');
 });
+
+/**
+* @swagger
+* /test:
+*   post:
+*      description: Use this to test the first-API route
+*      tags:
+*          - test
+*      parameters:
+*          - in: body
+*            name: none
+*            description: No input data
+*            schema:
+*              type: object
+*          - out: body
+*            name: response
+*            description: This is the test output response
+*            schema:
+*              type: object
+*              properties:
+*                  body:
+*                      type: string
+*                      minLength: 1
+*                      maxLength: 45
+*                      example: Test Successful. Welcome to the first-API backend!!
+*      responses:
+*          '200':
+ *             description: Test Successful
+*          '400':
+*              description: Bad Request
+*          '500':
+*              description: Internal Server Error
+*/
 
 // MAIN API ENDPOINTS
 app.use('/users', userRoutes);
